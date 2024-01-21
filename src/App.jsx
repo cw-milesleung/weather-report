08e4ba3a94fb60cb152e09f0ba0d90a5335e0e44
miles25 <miles@databased.com>
2024-01-20 17:25:31 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
-      <h1>Weather report</h1>
-    </>
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full">
+      Hello world
+    </main>
   );
 }
 
